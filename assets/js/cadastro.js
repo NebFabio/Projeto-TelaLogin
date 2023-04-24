63a2b3c6bef7b2ed0e5b7f1b1cb9400347f33b49
unnebell <128543369+NebFabio@users.noreply.github.com>
2023-04-23 22:51:47 -0300

    implementando ks

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -1,28 +1,35 @@
 let btn = document.querySelector('#verSenha')
 let btnConfirm = document.querySelector('#verConfirmSenha')
 
-btn.addEventListener('click', ()=>{
-    let inputSenha = document.querySelector('#isenha')
-    
-    if(inputSenha.getAttribute('type') == 'password'){
-      inputSenha.setAttribute('type', 'text')
-    } else {
-      inputSenha.setAttribute('type', 'password')
-    }
-  })
+btn.addEventListener('click', () => {
+  let inputSenha = document.querySelector('#isenha')
 
-  btnConfirm.addEventListener('click', ()=>{
-    let inputSenha = document.querySelector('#iconfirmSenha')
-    
-    if(inputSenha.getAttribute('type') == 'password'){
-      inputSenha.setAttribute('type', 'text')
-    } else {
-      inputSenha.setAttribute('type', 'password')
-    }
-  })
+  if (inputSenha.getAttribute('type') == 'password') {
+    inputSenha.setAttribute('type', 'text')
+  } else {
+    inputSenha.setAttribute('type', 'password')
+  }
+})
 
-  function cadastrar(){
-    alert('Cadastro feito com sucesso!')
+btnConfirm.addEventListener('click', () => {
+  let inputSenha = document.querySelector('#iconfirmSenha')
+
+  if (inputSenha.getAttribute('type') == 'password') {
+    inputSenha.setAttribute('type', 'text')
+  } else {
+    inputSenha.setAttribute('type', 'password')
+  }
+})
+
+function cadastrar() {
+  let nome = document.getElementById('inome').value;
+  let usu = document.getElementById('iusu').value;
+  let senha = document.getElementById('isenha').value;
+  let confirmSenha = document.getElementById('iconfirmSenha').value;
+
+  if (nome && usu && senha && confirmSenha != 0) {
+    alert('Cadastrado com sucesso!')
+  } else {
+    alert('Ops, faltando dados...')
+  }
 }
-
-  
